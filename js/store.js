3792c367f7da67f3cd41545c8c581272cdfb1486
Your Name <mouachek@student.42.fr>
2021-04-20 14:05:30 +0200

    delete console log, fix pb id’s, change if else in switch

--- js/store.js
+++ js/store.js
@@ -81,7 +81,7 @@
 		callback = callback || function () {};
 
 		// Generate an ID
-	    var newId = ""; 
+	    var newId = "";
 	    var charset = "0123456789";
 
         for (var i = 0; i < 6; i++) {
@@ -104,8 +104,8 @@
 		} else {
 
     		// Assign an ID
-			updateData.id = parseInt(newId);
-    
+			updateData.id = Date.now(); // Return unique ID selon la date
+
 
 			todos.push(updateData);
 			localStorage[this._dbName] = JSON.stringify(data);
@@ -123,7 +123,7 @@
 		var data = JSON.parse(localStorage[this._dbName]);
 		var todos = data.todos;
 		var todoId;
-		
+
 		for (var i = 0; i < todos.length; i++) {
 			if (todos[i].id == id) {
 				todoId = todos[i].id;
